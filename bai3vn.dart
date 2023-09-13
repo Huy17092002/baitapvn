72c1067921f5bbfb669d3770dddc2f668c5c1c8e
unknown <huylequang1792002@gmail.com>
2023-09-13 15:05:17 +0700

    bai1

--- bai3vn.dart
+++ bai3vn.dart
@@ -1,13 +1,1 @@
-int giaithua(int num) {
-  if (num <= 1) {
-    return 1;
-  } else {
-    return num * giaithua(num - 1);
-  }
-}
 
-void main() {
-  var num = 6;
-  var fact = giaithua(num);
-  print("Giai thừa $num là: ${fact}");
-}
