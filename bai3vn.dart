dd880f99908d8d0487f5874197f5ad2e469dac83
unknown <huylequang1792002@gmail.com>
2023-09-15 19:59:32 +0700

    bai4

--- bai3vn.dart
+++ bai3vn.dart
@@ -1,1 +1,17 @@
+//ý1
+extension NumberExtensions on num {
+  num add(num other) => this + other;
+  num subtract(num other) => this - other;
+  num divide(num other) => this / other;
+  num multiply(num other) => this * other;
+}
 
+void main() {
+  num a = 10;
+  num b = 2;
+
+  print('a + b = ${a.add(b)}');
+  print('a - b = ${a.subtract(b)}');
+  print('a / b = ${a.divide(b)}');
+  print('a * b = ${a.multiply(b)}');
+}
